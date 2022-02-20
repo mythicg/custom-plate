694ca201923289c3bcf8e10a3c617a489788713e
Astra <60952407+AstraWrld@users.noreply.github.com>
2022-02-20 00:10:20 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    "PLATE_PATTERN": "AAA 111"
+
+    //     1 will lead to a random number from 0-9.
+    // A will lead to a random letter from A-Z.
+    // . will lead to a random letter or number, with 50% probability of being either.
+    // ^1 will lead to a literal 1 being emitted.
+    // ^A will lead to a literal A being emitted.
+    // Any other character will lead to said character being emitted.
+    // A string shorter than 8 characters will be padded on the right.
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Astra#2100'
+
+client_script {
+    'src/client.js',
+    'config.js'
+} 
